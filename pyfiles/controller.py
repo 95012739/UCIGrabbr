9935305d7ab726acd547b59e7b45661f950e7677
95012739 <zach.segal@gmail.com>
2021-05-10 19:16:49 +0300

    COMMENT TO DESCRIBE THE INTENTION OF THE COMMIT

--- pyfiles/controller.py
+++ pyfiles/controller.py
@@ -1,15 +1,37 @@
 
 import os
-from interface import *
-from  worker import *
-from printr import *
+import uuid as ud
+import boto3 as b3
+import pandas
+import bs4 #beautifulsoup4
+import requests
+import functools
+import sys
+
+os.chdir("~/UCIGrabbr")
+sys.path.append("/uci/pyfiles")
+
+import interface
+import worker 
+import printr 
+import logging
+from botocore.exceptions import ClientError
+
+os.chdir("~/UCIGrabbr")
+sys.path.append("/uci/UCI-ML-API/")
+
+from UCI_ML_Functions import download_dataset_url, download_dataset_name
+
 directory = "seeds"
 cwd = os.getcwd()
 local_directory = cwd + "/uci" + str(directory)
+
 os.makedirs(local_directory)
-dataset, buck, instructions = worker.usr_input()
+
+dataset, buck, instructions = interface.usr_input()
 
 data = worker.datagrabbr(dataset)
-print.doit(data, buck)
+
+printr.doit(data, buck)
 
 
--- pyfiles/printr.py
+++ pyfiles/printr.py
@@ -1,48 +1,26 @@
-import uuid
-import boto3
-import pandas
-import beautifulsoup4
-import requests
-import functools
-
-def compose(*funcs):
-    """
-    Functional composition
-    
-    [f, g, h] will be f(g(h(x)))
-    """
-    def compose_two(f, g):
-        def c(x):
-            return f(g(x))
-        return c
-    return functools.reduce(compose_two, funcs)
-
-#pick one
 
 
 
-def doit(df, buck):
-    create_bucket(buck, s3_connection)
-    s3_client = boto3.client('s3')
-        Filename=data, Bucket=buck, #or create_bucket_name(buck)
-        Key=first_file_name) #?
+
+def doit(df, buck, object_name = None):
+    s3_client = b3.client('s3')
+    bucket_name = create_bucket_name(buck)
+    s3_client.create_bucket(Bucket=bucket_name)
+
+    if object_name is None:
+        object_name = "file"
+
+    response = s3_client.upload_file(df, bucket_name, object_name)
+
+    return True
 
 #creates an S3 bucket name 
 def create_bucket_name(bucket_prefix):
-    # The generated bucket name must be between 3 and 63 chars long
-    return ''.join([bucket_prefix, str(uuid.uuid4())])
+    i = 7
+#The generated bucket name must be between 3 and 63 chars long
+    return ''.join([bucket_prefix, str(ud.uuid4())])
 
-#creates the bucket
-def create_bucket(bucket_prefix, s3_connection):
-    session = boto3.session.Session()
-    current_region = session.region_name
-    bucket_name = create_bucket_name(bucket_prefix)
-    bucket_response = s3_connection.create_bucket(
-        Bucket=bucket_name,
-        CreateBucketConfiguration={
-        'LocationConstraint': current_region})
-    print(bucket_name, current_region)
-    return bucket_name, bucket_response
+
 
 #input name
 #create bucket bucket name

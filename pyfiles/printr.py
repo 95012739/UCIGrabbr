a34175bd44c338bcb31ee5ad8e91171dc328aa50
zachs9 <zach.segal@gmail.com>
2022-04-27 11:50:43 -0400

    Update printr.py

--- pyfiles/printr.py
+++ pyfiles/printr.py
@@ -1,19 +1,25 @@
-
+#Download some packages
 import uuid as ud
 import boto3 as b3
 import os
 import pandas
+
 def doit(df, buck, object_name = None):
+    
     df = pandas.DataFrame(df)
+    
     cwd = os.getcwd()
 
     path = cwd + "\mojdata.csv"
 
+    #make sure we have one path and create it if it doesn't exist
     if os.path.exists(path):
         os.remove(path)
 
+    #get our data in a csv with the right name
     df.to_csv(path)
 
+    #stick it in a bucket
     s3_client = b3.client('s3')
     bucket_name = create_bucket_name(buck)
     s3_client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={
@@ -21,8 +27,8 @@ def doit(df, buck, object_name = None):
 
     if object_name is None:
         object_name = "file"
-
-    #could do try and except ie already have name etc.
+        
+    #Upload
     s3_client.upload_file(path, bucket_name, object_name)
 
     return True
